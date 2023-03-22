dcd6efa11551fbd3b9485dd7626ae05307a7862c
mansisingh78tomar <118250409+mansisingh78tomar@users.noreply.github.com>
2023-03-22 13:40:12 +0530

    sxsa

--- 32/Accordion/main.js
+++
@@ -1,8 +0,0 @@
-const accordion = document.getElementsByClassName('accordion');
-
-
-for(let i = 0; i < accordion.length; i++){
-    accordion[i].addEventListener('click', function () {
-        this.classList.toggle('active');
-    });
-}
--- 36/Spotlight/main.js
+++
@@ -1,9 +0,0 @@
-$(document).mousemove(function (e) {
-    var x = e.pageX;
-    var y = e.pageY;
-    $(".spotlight").css(
-      "background",
-      "radial-gradient(circle at " + x + "px " + y + "px, transparent, #000 25%"
-    );
-  });
-  
